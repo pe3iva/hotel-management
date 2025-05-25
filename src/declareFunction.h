0d9495f621d7f73cd6b2915fd9a99ee9ea1f095c
pe3iva <dawgsofast@protonmail.com>
2025-05-25 22:18:19 +0200

    rewrite function header from if to switch statements

--- src/declareFunction.h
+++ src/declareFunction.h
@@ -1,21 +1,20 @@
 int goTo(int destination){
 
-    if(destination == 1){
+    switch(destination){
+        case 1: 
+            std::cout << "\n\nGUEST INFORMATION\n\n";
+        break;
 
-        std::cout << "\n\nGUEST INFORMATION\n\n";
+        case 2:
+            std::cout << "\n\nGUEST REQUESTS\n\n";
+        break;
 
-        
-    } else if(destination == 2){
-        
-        std::cout << "\n\nGUEST REQUESTS\n\n";
+        case 3:
+            std::cout << "\n\nGUEST APPROVAL\n\n";
+        break;
 
-    } else if(destination == 3){
-        
-        std::cout << "\n\nGUEST APPROVAL\n\n";
-
-    } else{
-        
-        std::cout << "\n\nWrong menu number entered, please choose a number from 1 to 3\n\n";
+        default:
+            std::cout << "\n\nWrong menu number entered, please choose a number from 1 to 3\n\n";
 
     }
 
